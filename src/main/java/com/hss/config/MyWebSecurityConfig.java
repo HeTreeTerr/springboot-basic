6deec3c4466b7399683a64cdb86b707970fcb293
hesensen <hesensen@hztianque.com>
2020-07-04 18:44:14 +0800

    个性化

--- src/main/java/com/hss/config/MyWebSecurityConfig.java
+++ src/main/java/com/hss/config/MyWebSecurityConfig.java
@@ -3,6 +3,8 @@ package com.hss.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,6 +20,38 @@ public class MyWebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
         auth.inMemoryAuthentication()
-                .withUser("admin").password("123").roles("ADMIN","USER");
+                .withUser("admin").password("123").roles("ADMIN")
+                .and()
+                .withUser("user").password("123").roles("USER");
     }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+                .antMatchers("/admin/**")
+                .hasRole("ADMIN")
+                .antMatchers("/user/**")
+                .hasRole("USER")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/loginPage")
+                .loginProcessingUrl("/login")
+                .usernameParameter("userName")
+                .passwordParameter("password")
+                .defaultSuccessUrl("/indexPage",true)
+                .failureUrl("/loginPage")
+                .permitAll()
+                .and()
+                .csrf()
+                .disable();
+    }
+
+   @Override
+  public void configure(WebSecurity web) throws Exception {
+      //解决静态资源被拦截的问题
+      web.ignoring().antMatchers("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg"
+                                ,"/**/*.ico","/**/*.svg");
+  }
 }
